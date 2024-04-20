633bbf083bc88451134b3d954db8b4f32ed3987b
Dave <dave.cadelina@outlook.com>
2024-04-21 01:43:16 +1000

    uuid

--- src/tasks/task.model.ts
+++ src/tasks/task.model.ts
@@ -1,11 +1,11 @@
 export interface Task {
-  id: number;
-  name: string;
+  id: string;
+  title: string;
   description: string;
   status: TaskStatus;
 }
 
-enum TaskStatus {
+export enum TaskStatus {
   OPEN = 'OPEN',
   IN_PROGRESS = 'IN_PROGRESS',
   DONE = 'DONE',
