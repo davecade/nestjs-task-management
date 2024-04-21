fe859e605f11e7aead9a53e41e11d76cf1768eb6
Dave <dave.cadelina@outlook.com>
2024-04-21 14:16:10 +1000

    added DTOs

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -12,7 +13,7 @@ export class TasksController {
   }
 
   @Post()
-  creatTask(@Body() body) {
-    console.log('body >> ', body);
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
   }
 }
